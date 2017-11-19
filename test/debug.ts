4b6c085e897601a561b82504a8e4ab4706da315d
houjun <freebirds8281@hotmail.com>
2017-11-20 01:08:08 +0800

    comment

--- test/debug.ts
+++ test/debug.ts
@@ -1,97 +1,97 @@
-// import { AutoWired, Injectable, Param, Singleton, Registration, Inject } from '../src';
+import { AutoWired, Injectable, Param, Singleton, Registration, Inject } from '../src';
 
-// export class SimppleAutoWried {
-//     constructor() {
-//     }
+export class SimppleAutoWried {
+    constructor() {
+    }
 
-//     @AutoWired
-//     dateProperty: Date;
-// }
+    @AutoWired
+    dateProperty: Date;
+}
 
-// @Singleton
-// export class Person {
-//     name = 'testor';
-// }
+@Singleton
+export class Person {
+    name = 'testor';
+}
 
-// @Singleton
-// @Injectable
-// export class RoomService {
-//     constructor() {
+@Singleton
+@Injectable
+export class RoomService {
+    constructor() {
 
-//     }
-//     @AutoWired
-//     current: Date;
-// }
+    }
+    @AutoWired
+    current: Date;
+}
 
-// @Injectable()
-// export class ClassRoom {
-//     constructor(public service: RoomService) {
+@Injectable()
+export class ClassRoom {
+    constructor(public service: RoomService) {
 
-//     }
-// }
+    }
+}
 
-// export abstract class Student {
-//     constructor() {
-//     }
-//     abstract sayHi(): string;
-// }
+export abstract class Student {
+    constructor() {
+    }
+    abstract sayHi(): string;
+}
 
-// @Injectable({ provide: Student })
-// export class MiddleSchoolStudent extends Student {
-//     constructor() {
-//         super();
-//     }
-//     sayHi() {
-//         return 'I am a middle school student';
-//     }
-// }
+@Injectable({ provide: Student })
+export class MiddleSchoolStudent extends Student {
+    constructor() {
+        super();
+    }
+    sayHi() {
+        return 'I am a middle school student';
+    }
+}
 
-// @Injectable()
-// export class MClassRoom {
-//     @AutoWired({ type: MiddleSchoolStudent })
-//     leader: Student;
-//     constructor() {
+@Injectable()
+export class MClassRoom {
+    @AutoWired({ type: MiddleSchoolStudent })
+    leader: Student;
+    constructor() {
 
-//     }
-// }
-
-
-// @Injectable({ provide: Student, alias: 'college' })
-// export class CollegeStudent extends Student {
-//     constructor() {
-//         super();
-//     }
-//     sayHi() {
-//         return 'I am a college student';
-//     }
-// }
-
-// @Injectable
-// export class CollegeClassRoom {
-//     constructor(
-//         @AutoWired({ type: CollegeStudent })
-//         @Param({ type: CollegeStudent })
-//         public leader: Student) {
-
-//     }
-// }
+    }
+}
 
 
-// @Injectable
-// export class InjMClassRoom {
-//     @Inject // @Inject({ type: MiddleSchoolStudent })
-//     leader: Student;
-//     constructor() {
+@Injectable({ provide: Student, alias: 'college' })
+export class CollegeStudent extends Student {
+    constructor() {
+        super();
+    }
+    sayHi() {
+        return 'I am a college student';
+    }
+}
 
-//     }
-// }
+@Injectable
+export class CollegeClassRoom {
+    constructor(
+        @AutoWired({ type: CollegeStudent })
+        @Param({ type: CollegeStudent })
+        public leader: Student) {
+
+    }
+}
 
 
-// @Injectable
-// export class InjCollegeClassRoom {
-//     constructor(
-//         @Inject({ type: CollegeStudent })
-//         public leader: Student) {
+@Injectable
+export class InjMClassRoom {
+    @Inject // @Inject({ type: MiddleSchoolStudent })
+    leader: Student;
+    constructor() {
 
-//     }
-// }
+    }
+}
+
+
+@Injectable
+export class InjCollegeClassRoom {
+    constructor(
+        @Inject({ type: CollegeStudent })
+        public leader: Student) {
+
+    }
+}
