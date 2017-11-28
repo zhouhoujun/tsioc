c7ea6f06a5da48d69daa83d05bb3c51b4414958b
zhouhoujun <freebirds8281@hotmail.com>
2017-11-28 14:49:13 +0800

    bug

--- src/simples/debug.ts
+++ src/simples/debug.ts
@@ -1,4 +1,4 @@
-import { AutoWired, Injectable, Param, Singleton, Inject, Registration, ContainerBuilder, Aspect } from '../index';
+import { AutoWired, Injectable, Param, Singleton, Inject, Registration, ContainerBuilder, Aspect, Method } from '../index';
 
 
 export class SimppleAutoWried {
@@ -170,7 +170,7 @@ class MethodTestPerson {
 
 class MethodTest {
 
-    @Runner
+    @Method
     sayHello(person: MethodTestPerson) {
         return person.say();
     }
