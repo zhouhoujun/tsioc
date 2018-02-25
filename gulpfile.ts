f339daf977de132672963cc5c61e9275f4d8b137
zhj <freebirds8281@hotmail.com>
2018-02-25 13:21:51 +0800

    ts-node 5 has mocha test bug

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,7 @@
 import * as gulp from 'gulp';
 import { ITaskOption, Development } from 'development-tool';
 import { Operation } from 'development-core';
-import { classAnnotations } from 'typescript-class-annotations'
+// import { classAnnotations } from 'typescript-class-annotations'
 // import 'development-tool-node';
 const resolve = require('rollup-plugin-node-resolve');
 const rollupSourcemaps = require('rollup-plugin-sourcemaps');
