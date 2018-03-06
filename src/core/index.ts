49e41da380dee080bcd52023b1ec313857cbf7c5
houjun <freebirds8281@hotmail.com>
2018-03-06 23:19:47 +0800

    add default Boolean register

--- src/core/index.ts
+++ src/core/index.ts
@@ -71,5 +71,6 @@ export function registerCores(container: IContainer) {
     container.register(Date, () => new Date());
     container.register(String, () => '');
     container.register(Number, () => Number.NaN);
+    container.register(Boolean, () => undefined);
 
 }
