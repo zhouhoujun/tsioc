c6e24f4f54673dd2ccd78987310e51f32aff47ef
zhouhoujun <freebirds8281@hotmail.com>
2018-12-11 16:55:25 +0800

    core module

--- packages/activities/core/src/CoreModule.ts
+++ packages/activities/core/src/CoreModule.ts
@@ -1,12 +1,11 @@
-import { IContainer, CoreActions, Inject, ContainerToken, IocExt, MetaAccessorToken } from '@ts-ioc/core';
+import { IContainer, CoreActions, Inject, ContainerToken, IocExt } from '@ts-ioc/core';
 import { Task, Workflow } from './decorators';
 import { RunAspect } from './aop';
 import * as injectors from './injectors';
 import * as activites from './activities';
 import * as core from './core';
 import { InputDataToken } from './core';
-import { ActivityMetaAccessorToken } from './injectors';
-import { ApplicationBuilderToken } from '@ts-ioc/bootstrap';
+
 /**
  * register task decorators.
  *
@@ -25,10 +24,6 @@ export class CoreModule {
         lifeScope.registerDecorator(Task, CoreActions.bindProvider, CoreActions.cache, CoreActions.componentBeforeInit, CoreActions.componentInit, CoreActions.componentAfterInit);
 
         container.bindProvider(InputDataToken, null);
-        // container.bindTarget(ApplicationBuilderToken, [
-        //     { provide: MetaAccessorToken, useExisting: ActivityMetaAccessorToken }
-        // ]);
-        // container.bindRefProvider(ApplicationBuilderToken, MetaAccessorToken, ActivityMetaAccessorToken);
         container.use(injectors)
             .use(core)
             .use(RunAspect)
