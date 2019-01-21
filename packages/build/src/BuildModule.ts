46cc941607ae9088ea88110962dde951b9a5e6f3
houjun <freebirds8282@hotmail.com>
2019-01-21 16:38:40 +0800

    remove import

--- packages/build/src/BuildModule.ts
+++ packages/build/src/BuildModule.ts
@@ -2,18 +2,18 @@ import { DIModule } from '@ts-ioc/bootstrap';
 import * as buildcore from './core';
 import { ServerActivitiesModule } from '@ts-ioc/platform-server-activities';
 import { AssetSetup } from './AssetSetup';
-import { ServerLogsModule } from '@ts-ioc/platform-server-logs';
+// import { ServerLogsModule } from '@ts-ioc/platform-server-logs';
 
 
 @DIModule({
     imports: [
         AssetSetup,
-        ServerLogsModule,
+        // ServerLogsModule,
         ServerActivitiesModule,
         buildcore
     ],
     exports: [
-        ServerLogsModule,
+        // ServerLogsModule,
         ServerActivitiesModule,
         buildcore
     ]
