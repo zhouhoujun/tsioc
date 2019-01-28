a719eca434e3a5a3d4e4f3df2c5a7bc0172feee4
houjun <freebirds8282@hotmail.com>
2019-01-28 15:03:24 +0800

    update configure register.

--- packages/platform-server-activities/src/WorkflowConfigureRegister.ts
+++ packages/platform-server-activities/src/WorkflowConfigureRegister.ts
@@ -1,14 +1,14 @@
 import { ConfigureRegister, RunnableConfigure } from '@ts-ioc/bootstrap';
-import { IContainer, Singleton } from '@ts-ioc/core';
+import { Singleton } from '@ts-ioc/core';
 import { DebugLogAspect } from '@ts-ioc/logs';
 
 
 @Singleton
 export class WorkflowConfigureRegister extends ConfigureRegister<RunnableConfigure> {
 
-    async register(config: RunnableConfigure, container: IContainer): Promise<void> {
+    async register(config: RunnableConfigure): Promise<void> {
         if (config.debug) {
-            container.register(DebugLogAspect);
+            this.container.register(DebugLogAspect);
         }
     }
 }
