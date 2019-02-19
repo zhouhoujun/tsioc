162a39e7262bfc2dc53d2f698cda24282dccdf35
houjun <freebirds8281@hotmail.com>
2019-02-19 21:51:14 +0800

    remove unused build task

--- packages/build/src/shells/IShellAssetActivity.ts
+++ packages/build/src/shells/IShellAssetActivity.ts
@@ -1,9 +1,18 @@
 import { InjectAssetToken, AssetConfigure } from '../core';
 
+/**
+ * shell asset token.
+ */
 export const ShellAssetToken = new InjectAssetToken('shell-asset');
 
 
-
+/**
+ * shell asset configure.
+ *
+ * @export
+ * @interface ShellAssetConfigure
+ * @extends {AssetConfigure}
+ */
 export interface ShellAssetConfigure extends AssetConfigure {
 
 }
