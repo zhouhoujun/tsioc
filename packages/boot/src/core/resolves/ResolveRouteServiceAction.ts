a78ea02f220913269966852b0ba8f402fd8dcdea
houjun <freebirds8281@hotmail.com>
2019-04-21 23:01:24 +0800

    check route resolver service

--- packages/boot/src/core/resolves/ResolveRouteServiceAction.ts
+++ packages/boot/src/core/resolves/ResolveRouteServiceAction.ts
@@ -2,17 +2,22 @@ import { ResolveServiceContext } from '@tsdi/core';
 import { IocCompositeAction, Singleton, Autorun } from '@tsdi/ioc';
 import { ResolveModuleExportAction } from './ResolveModuleExportAction';
 import { ResolveParentServiceAction } from './ResolveParentServiceAction';
+import { ContainerPool } from '../ContainerPool';
 
 
 @Singleton
 @Autorun('setup')
 export class ResolveRouteServiceAction extends IocCompositeAction<ResolveServiceContext<any>>  {
     execute(ctx: ResolveServiceContext<any>, next?: () => void): void {
-        let token = ctx.token;
-        ctx.token = ctx.currToken || ctx.token;
-        super.execute(ctx);
-        if (!ctx.instance) {
-            ctx.token = token;
+        if (this.container.has(ContainerPool)) {
+            let token = ctx.token;
+            ctx.token = ctx.currToken || ctx.token;
+            super.execute(ctx);
+            if (!ctx.instance) {
+                ctx.token = token;
+                next && next();
+            }
+        } else {
             next && next();
         }
     }
--- packages/boot/src/core/resolves/ResolveRouteServicesAction.ts
+++ packages/boot/src/core/resolves/ResolveRouteServicesAction.ts
@@ -2,17 +2,22 @@ import { ResolveServicesContext } from '@tsdi/core';
 import { IocCompositeAction, Singleton, Autorun } from '@tsdi/ioc';
 import { ResolveSerivesInExportAction } from './ResolveSerivesInExportAction';
 import { ResolveParentServicesAction } from './ResolveParentServicesAction';
+import { ContainerPool } from '../ContainerPool';
 
 
 @Singleton
 @Autorun('setup')
 export class ResolveRouteServicesAction extends IocCompositeAction<ResolveServicesContext<any>>  {
     execute(ctx: ResolveServicesContext<any>, next?: () => void): void {
-        let token = ctx.token;
-        ctx.token = ctx.currToken || ctx.token;
-        super.execute(ctx);
-        if (!ctx.instance) {
-            ctx.token = token;
+        if (this.container.has(ContainerPool)) {
+            let token = ctx.token;
+            ctx.token = ctx.currToken || ctx.token;
+            super.execute(ctx);
+            if (!ctx.instance) {
+                ctx.token = token;
+                next && next();
+            }
+        } else {
             next && next();
         }
     }
