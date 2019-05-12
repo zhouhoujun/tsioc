c16624464d1529e74b74118fbc75c0350eb4a750
houjun <freebirds8281@hotmail.com>
2019-05-12 13:16:35 +0800

    task

--- packages/annotations/taskfile.ts
+++ packages/annotations/taskfile.ts
@@ -1,20 +1,29 @@
-import { PackModule, Pack, PackActivity } from '@tsdi/pack';
-import { Workflow } from '@tsdi/activities';
+import { Workflow, Task } from '@tsdi/activities';
+import { TsBuildOption, PackModule } from '@tsdi/pack';
+import { ServerActivitiesModule } from '@tsdi/platform-server-activities';
 
-@Pack({
+@Task({
+    deps: [
+        PackModule,
+        ServerActivitiesModule
+    ],
     baseURL: __dirname,
-    src: 'src',
-    clean: 'lib',
-    test: 'test/**/*.spec.ts',
-    assets: {
-        ts: { dest: 'lib', annotation: true, uglify: false }
+    template: <TsBuildOption>{
+        activity: 'ts',
+        clean: ['../../dist/annotations/lib'],
+        src: 'src/**/*.ts',
+        dist: '../../dist/annotations/lib',
+        annotation: true,
+        sourcemaps: './sourcemaps',
+        jsValuePipe: true,
+        uglify: true,
+        tsconfig: './tsconfig.json'
+
     }
 })
-export class AnnoBuilder {
+export class AnnotationsBuild {
 }
 
 if (process.cwd() === __dirname) {
-    Workflow.create()
-        .use(PackModule)
-        .bootstrap(AnnoBuilder);
+    Workflow.run(AnnotationsBuild);
 }
