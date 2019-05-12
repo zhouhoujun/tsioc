7352336ea47460b5294bf62548dc010a39acdef0
houjun <freebirds8281@hotmail.com>
2019-05-12 17:57:03 +0800

    update

--- packages/boot/taskfile.ts
+++ packages/boot/taskfile.ts
@@ -1,108 +1,209 @@
-import { Pack, PackActivity, PackModule } from '@tsdi/pack';
-import { Workflow } from '@tsdi/activities';
-import { Asset, AssetActivity, TsCompile, CleanToken, TransformContext } from '@tsdi/build';
-import { IActivity } from '@tsdi/activities';
+import {  PackModule, TsBuildOption } from '@tsdi/pack';
+import { Workflow, Task } from '@tsdi/activities';
+import { ServerActivitiesModule } from '@tsdi/platform-server-activities';
 const resolve = require('rollup-plugin-node-resolve');
 const rollupSourcemaps = require('rollup-plugin-sourcemaps');
 const commonjs = require('rollup-plugin-commonjs');
 const rollup = require('gulp-rollup');
 const rename = require('gulp-rename');
 
-@Asset({
-    src: 'lib/**/*.js',
-    dest: 'bundles',
-    data: {
-        name: 'bootstrap.umd.js',
-        input: 'lib/index.js',
-        format: 'umd'
-    },
-    sourcemaps: true,
-    pipes: [
-        (ctx: TransformContext) => rollup({
-            name: ctx.config.data.name,
-            format: ctx.config.data.format || 'umd',
-            sourceMap: true,
-            plugins: [
-                resolve(),
-                commonjs(),
-                rollupSourcemaps()
-            ],
-            external: [
-                'reflect-metadata',
-                'events',
-                'tslib',
-                '@tsdi/core',
-                '@tsdi/aop'
-            ],
-            globals: {
-                'reflect-metadata': 'Reflect',
-                'tslib': 'tslib',
-                '@tsdi/core': '@tsdi/core',
-                '@tsdi/aop': '@tsdi/aop'
-            },
-            input: ctx.relativeRoot(ctx.config.data.input)
-        }),
-        (ctx) => rename(ctx.config.data.name)
-    ]
-})
-export class BootRollup {
-}
 
-@Pack({
+
+@Task({
+    deps: [
+        PackModule,
+        ServerActivitiesModule
+    ],
     baseURL: __dirname,
-    src: 'src',
-    clean: ['lib', 'bundles', 'fesm5', 'es2015', 'fesm2015'],
-    test: (ctx) => ctx.getEnvArgs().test === 'false' ? '' : 'test/**/*.spec.ts',
-    assets: {
-        ts: {
-            sequence: [
-                { src: 'src/**/*.ts', dest: 'lib', annotation: true, uglify: false, activity: TsCompile },
-                BootRollup,
-                {
-                    name: 'zip',
-                    src: 'bundles/bootstrap.umd.js',
-                    dest: 'bundles',
-                    sourcemaps: true,
-                    uglify: true,
-                    pipes: [
-                        () => rename('bootstrap.umd.min.js')
-                    ],
-                    task: AssetActivity
-                },
-                {
-                    src: 'lib/**/*.js', dest: 'fesm5',
-                    data: {
-                        name: 'bootstrap.js',
-                        input: 'lib/index.js',
-                        format: 'cjs'
-                    },
-                    activity: BootRollup
-                }
-            ]
-        },
-        ts2015: {
-            sequence: [
-                { src: 'src/**/*.ts', dest: 'es2015', tds: false, annotation: true, uglify: false, tsconfig: './tsconfig.es2015.json', activity: TsCompile },
-                {
-                    src: 'es2015/**/*.js',
-                    dest: 'fesm2015',
-                    data: {
-                        name: 'bootstrap.js',
-                        input: './es2015/index.js',
-                        format: 'cjs'
-                    },
-                    activity: BootRollup
-                }
-            ]
-        }
+    template: {
+        activity: 'each',
+        each: [
+            { clean: ['../../dist/boot/lib'], dist: '../../dist/boot/lib', uglify: true, tsconfig: './tsconfig.json' },
+            // { clean: ['../../dist/boot/fesm2015'], dist: '../../dist/boot/fesm2015', uglify: true, tsconfig: './tsconfig.es2015.json' }
+        ],
+        body: [
+            <TsBuildOption>{
+                activity: 'ts',
+                // clean: ['lib', 'bundles', 'fesm5', 'es2015', 'fesm2015'],
+                src: 'src/**/*.ts',
+                test: 'test/**/*.ts',
+                annotation: true,
+                sourcemaps: './sourcemaps',
+                jsValuePipe: true,
+                tsconfig: ctx => ctx.body.tsconfig,
+                uglify: ctx => ctx.body.uglify,
+                dist: ctx => ctx.body.dist,
+                clean: ctx => ctx.body.clean
+            },
+            // <RollupOption>{
+            //     activity: 'rollup',
+            //     // annoation: true,
+            //     // ts: ctx => {
+            //     //     return {
+            //     //         tsconfig: ctx.body.tsconfig
+            //     //     }
+            //     // },
+            //     options: ctx => {
+            //         return {
+            //             input: 'src/index.ts',
+            //             plugins: [
+            //                 resolve(),
+            //                 // {
+            //                 //     resolveId(id) {
+            //                 //         if (/^\./.test(id)) {
+            //                 //             return id;
+            //                 //         }
+            //                 //         return null;
+            //                 //     }
+            //                 // },
+            //                 // commonjs({
+            //                 //     exclude: ['node_modules/**', '../../node_modules/**']
+            //                 // }),
+            //                 rollupClassAnnotations(),
+            //                 ts(),
+            //                 builtins(),
+            //                 rollupSourcemaps()
+            //             ],
+            //             output: {
+            //                 file: `${ctx.body.dist}/pack.js`
+            //             },
+            //             external: [
+            //                 'reflect-metadata',
+            //                 'tslib',
+            //                 'globby', 'path', 'fs', 'events', 'stream', 'child_process',
+            //                 '@tsdi/ioc',
+            //                 '@tsdi/core',
+            //                 '@tsdi/aop',
+            //                 '@tsdi/logs',
+            //                 '@tsdi/boot',
+            //                 '@tsdi/unit',
+            //                 '@tsdi/platform-server',
+            //                 'minimist', 'gulp-sourcemaps', 'vinyl-fs', 'del', 'chokidar',
+            //                 'gulp-uglify', 'execa', '@tsdi/annotations', 'gulp-typescript',
+            //                 '@tsdi/activities',
+            //                 '@tsdi/platform-server-activities',
+            //                 'rxjs',
+            //                 'rxjs/operators'
+            //             ],
+            //             globals: {
+            //                 'reflect-metadata': 'Reflect',
+            //                 'tslib': 'tslib',
+            //                 'path': 'path',
+            //                 '@tsdi/ioc': '@tsdi/ioc',
+            //                 '@tsdi/core': '@tsdi/core',
+            //                 '@tsdi/aop': '@tsdi/aop',
+            //                 '@tsdi/boot': '@tsdi/boot',
+            //                 '@tsdi/activities': '@tsdi/activities'
+            //             },
+            //         }
+            //     }
+
+            // }
+        ]
     }
 })
 export class BootBuilder {
 }
 
 if (process.cwd() === __dirname) {
-    Workflow.create({ debug: false })
-        .use(PackModule)
-        .bootstrap(BootBuilder);
+    Workflow.run(BootBuilder);
 }
+
+// @Asset({
+//     src: 'lib/**/*.js',
+//     dest: 'bundles',
+//     data: {
+//         name: 'bootstrap.umd.js',
+//         input: 'lib/index.js',
+//         format: 'umd'
+//     },
+//     sourcemaps: true,
+//     pipes: [
+//         (ctx: TransformContext) => rollup({
+//             name: ctx.config.data.name,
+//             format: ctx.config.data.format || 'umd',
+//             sourceMap: true,
+//             plugins: [
+//                 resolve(),
+//                 commonjs(),
+//                 rollupSourcemaps()
+//             ],
+//             external: [
+//                 'reflect-metadata',
+//                 'events',
+//                 'tslib',
+//                 '@tsdi/core',
+//                 '@tsdi/aop'
+//             ],
+//             globals: {
+//                 'reflect-metadata': 'Reflect',
+//                 'tslib': 'tslib',
+//                 '@tsdi/core': '@tsdi/core',
+//                 '@tsdi/aop': '@tsdi/aop'
+//             },
+//             input: ctx.relativeRoot(ctx.config.data.input)
+//         }),
+//         (ctx) => rename(ctx.config.data.name)
+//     ]
+// })
+// export class BootRollup {
+// }
+
+// @Pack({
+//     baseURL: __dirname,
+//     src: 'src',
+//     clean: ['lib', 'bundles', 'fesm5', 'es2015', 'fesm2015'],
+//     test: (ctx) => ctx.getEnvArgs().test === 'false' ? '' : 'test/**/*.spec.ts',
+//     assets: {
+//         ts: {
+//             sequence: [
+//                 { src: 'src/**/*.ts', dest: 'lib', annotation: true, uglify: false, activity: TsCompile },
+//                 BootRollup,
+//                 {
+//                     name: 'zip',
+//                     src: 'bundles/bootstrap.umd.js',
+//                     dest: 'bundles',
+//                     sourcemaps: true,
+//                     uglify: true,
+//                     pipes: [
+//                         () => rename('bootstrap.umd.min.js')
+//                     ],
+//                     task: AssetActivity
+//                 },
+//                 {
+//                     src: 'lib/**/*.js', dest: 'fesm5',
+//                     data: {
+//                         name: 'bootstrap.js',
+//                         input: 'lib/index.js',
+//                         format: 'cjs'
+//                     },
+//                     activity: BootRollup
+//                 }
+//             ]
+//         },
+//         ts2015: {
+//             sequence: [
+//                 { src: 'src/**/*.ts', dest: 'es2015', tds: false, annotation: true, uglify: false, tsconfig: './tsconfig.es2015.json', activity: TsCompile },
+//                 {
+//                     src: 'es2015/**/*.js',
+//                     dest: 'fesm2015',
+//                     data: {
+//                         name: 'bootstrap.js',
+//                         input: './es2015/index.js',
+//                         format: 'cjs'
+//                     },
+//                     activity: BootRollup
+//                 }
+//             ]
+//         }
+//     }
+// })
+// export class BootBuilder {
+// }
+
+// if (process.cwd() === __dirname) {
+//     Workflow.create({ debug: false })
+//         .use(PackModule)
+//         .bootstrap(BootBuilder);
+// }
 
