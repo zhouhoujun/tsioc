752a53c949e51ea307681ed517a9c4cd071ad0c0
houjun <freebirds8282@hotmail.com>
2019-05-27 09:51:22 +0800

    config

--- packages/boot/src/builder/BootConfigureRegisterHandle.ts
+++ packages/boot/src/builder/BootConfigureRegisterHandle.ts
@@ -1,13 +1,15 @@
 import { BootHandle } from './BootHandle';
 import { BootContext } from '../BootContext';
-import { ConfigureRegister } from '../annotations';
+import { ConfigureRegister, ConfigureManager } from '../annotations';
 
 
 export class BootConfigureRegisterHandle extends BootHandle {
     async execute(ctx: BootContext, next: () => Promise<void>): Promise<void> {
         let regs = ctx.getRaiseContainer().getServices(ConfigureRegister, ctx.module);
         if (regs && regs.length) {
-            await Promise.all(regs.map(reg => reg.register(ctx.annoation, ctx)));
+            let mgr = this.resolve(ctx, ConfigureManager);
+            let config = await mgr.getConfig();
+            await Promise.all(regs.map(reg => reg.register(config, ctx)));
         }
         await next();
     }
