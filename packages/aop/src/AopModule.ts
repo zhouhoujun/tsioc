93893254e9d4d8348a9d3c67b996284fb909be09
houjun <freebirds8282@hotmail.com>
2019-06-10 15:10:33 +0800

    update

--- packages/aop/src/AopModule.ts
+++ packages/aop/src/AopModule.ts
@@ -1,7 +1,7 @@
 import {
     Inject, IocBeforeConstructorScope, IocAfterConstructorScope, IocContainerToken, IIocContainer, Autorun,
-    RuntimeMethodScope, DesignDecoratorRegisterer, BindProviderAction, RuntimeDecoratorRegisterer, RegisterSingletionAction,
-    DecoratorScopes, RuntimeLifeScope, ConstructorArgsAction, DesignAnnoationScope
+    RuntimeMethodScope, DesignDecoratorRegisterer, BindProviderAction, RuntimeDecoratorRegisterer,
+    RegisterSingletionAction, DecoratorScopes, RuntimeLifeScope, ConstructorArgsAction
 } from '@tsdi/ioc';
 import { Aspect } from './decorators/Aspect';
 import { Advisor } from './Advisor';
