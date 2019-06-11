ca4c61ed6491d329ac9e79160fdbe42fe8f96a3a
houjun <freebirds8281@hotmail.com>
2019-06-11 18:15:03 +0800

    comment

--- packages/boot/src/builder/ModuleBuilderLifeScope.ts
+++ packages/boot/src/builder/ModuleBuilderLifeScope.ts
@@ -2,7 +2,13 @@ import { BuildHandles, AnnoationContext } from '../core';
 import { RegisterModuleScope } from './RegisterModuleScope';
 import { ModuleBuildScope } from './ModuleBuildScope';
 
-
+/**
+ * module build life scope.
+ *
+ * @export
+ * @class ModuleBuilderLifeScope
+ * @extends {BuildHandles<AnnoationContext>}
+ */
 export class ModuleBuilderLifeScope extends BuildHandles<AnnoationContext> {
 
     setup() {
