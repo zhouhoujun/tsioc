1397b117d5dd9f3ffffb961933a2c7d388f4e1d0
zhouhoujun <freebirds8281@hotmail.com>
2019-09-05 17:21:34 +0800

    configure merge

--- packages/boot/src/builder/BootConfigureRegisterHandle.ts
+++ packages/boot/src/builder/BootConfigureRegisterHandle.ts
@@ -12,11 +12,11 @@ import { LogConfigureToken } from '@tsdi/logs';
  */
 export class BootConfigureRegisterHandle extends BootHandle {
     async execute(ctx: BootContext, next: () => Promise<void>): Promise<void> {
+        let mgr = this.resolve(ctx, ConfigureManager);
+        let config = await mgr.getConfig();
+        config = ctx.configuration = Object.assign({}, config, ctx.annoation);
         let regs = ctx.getRaiseContainer().getServices(ConfigureRegister);
         if (regs && regs.length) {
-            let mgr = this.resolve(ctx, ConfigureManager);
-            let config = await mgr.getConfig();
-            config = ctx.configuration = Object.assign({}, config, ctx.annoation);
             await Promise.all(regs.map(reg => reg.register(config, ctx)));
             if (config.logConfig && !this.container.has(LogConfigureToken) && !ctx.getRaiseContainer().has(LogConfigureToken)) {
                 this.container.bindProvider(LogConfigureToken, config.logConfig);
