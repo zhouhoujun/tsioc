07e869e2aea78f8b3d4ebfe72d549961162eccfb
zhouhoujun <freebirds8281@hotmail.com>
2019-09-19 08:10:40 +0800

    update

--- packages/activities/src/activities/ExpressionActivity.ts
+++ packages/activities/src/activities/ExpressionActivity.ts
@@ -20,14 +20,17 @@ export class ExpressionActivity<T> extends ControlerActivity<T> {
     @Input() expression: Expression<T>;
 
     protected async execute(ctx: ActivityContext): Promise<void> {
+        let expression;
         if (isString(this.expression)) {
             try {
                 // tslint:disable-next-line:no-eval
-                this.result.value = eval(this.expression);
+                expression = eval(this.expression);
             } catch { }
         } else {
-            this.result.value = await this.resolveExpression(this.expression, ctx);
+            expression = this.expression;
         }
+
+        this.result.value = await this.resolveExpression(expression, ctx);
     }
 
 }
