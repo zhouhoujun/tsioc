084afb0cf23eff55a7cfbd27c27b76c87d9fb428
houhou <freebirds8281@hotmail.com>
2020-01-07 08:24:08 +0800

    cache

--- packages/unit/src/runner/UnitTestRunner.ts
+++ packages/unit/src/runner/UnitTestRunner.ts
@@ -46,7 +46,7 @@ export class UnitTestRunner extends Runnable<any, UnitTestContext> {
         oldRunner.unregisterGlobalScope();
         await oldRunner.run();
         let builder = injector.resolve(BuilderService);
-        await PromiseUtil.step(suites.filter(v => isClass(v) && this.context.reflects.hasMetadata(Suite, v)).map(s => () => builder.run({ type: s, injector: this.context.injector })));
+        await PromiseUtil.step(suites.filter(v => isClass(v) && this.context.reflects.hasMetadata(Suite, v)).map(s => () => builder.run({ type: s, injector: injector })));
         await injector.resolve(TestReport).report();
     }
 }
