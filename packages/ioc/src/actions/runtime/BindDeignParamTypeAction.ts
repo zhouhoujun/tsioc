f1c3a1b3316fae8d79541a5af40ee3de98d1ae1d
houhou <freebirds8281@hotmail.com>
2020-01-08 16:09:11 +0800

    update

--- packages/ioc/src/actions/runtime/BindDeignParamTypeAction.ts
+++ packages/ioc/src/actions/runtime/BindDeignParamTypeAction.ts
@@ -17,3 +17,4 @@ export const BindDeignParamTypeAction = function (ctx: RuntimeActionContext, nex
     }
     next();
 };
+
--- packages/ioc/src/actions/runtime/BindParameterTypeAction.ts
+++ packages/ioc/src/actions/runtime/BindParameterTypeAction.ts
@@ -94,4 +94,3 @@ export const BindParameterTypeAction = function (ctx: RuntimeActionContext, next
 
     next();
 };
-
