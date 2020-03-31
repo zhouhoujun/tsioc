728b1f581c319a3ae4ba0239c1de140748616faa
houhou <freebirds8281@hotmail.com>
2020-03-31 19:11:31 +0800

    update

--- packages/compiler/src/source.ts
+++ packages/compiler/src/source.ts
@@ -11,7 +11,7 @@ export class SourceFile {
  * location in scource file.
  */
 export class SourceLocation {
-    constructor(source: SourceFile, public offset: number, public line: number, public col: number) {
+    constructor(public source: SourceFile, public offset: number, public line: number, public col: number) {
     }
 }
 
@@ -20,6 +20,10 @@ export class SourceLocation {
  */
 export class ParseLocation {
     constructor(public start: SourceLocation, public end: SourceLocation, detail?: string) {
+    }
+
+    toString(): string {
+        return this.start.source.content.substring(this.start.offset, this.end.offset);
     }
 }
 
