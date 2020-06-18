2562b5629daea7d05c80ce3a9988f5eb617b7835
houjun <freebirds8281@hotmail.com>
2020-06-19 07:41:53 +0800

    refactor boot message

--- packages/boot/src/registers/MessageRegisterAction.ts
+++ packages/boot/src/registers/MessageRegisterAction.ts
@@ -4,24 +4,26 @@ import { MessageMetadata } from '../decorators/Message';
 
 
 export const MessageRegisterAction = function (ctx: DesignContext, next: () => void): void {
-    let injector = ctx.injector;
-    let msgQueue = injector.getInstance(RootMessageQueueToken);
     let metas = ctx.reflects.getMetadata<MessageMetadata>(ctx.getValue(CTX_CURR_DECOR), ctx.type);
-    let { regIn, before, after } = metas.find(meta => !!meta.before || !!meta.after) || <MessageMetadata>{};
+    const { regIn, before, after } = metas.find(meta => !!meta.before || !!meta.after) || <MessageMetadata>{};
+    if (regIn === 'none') {
+        return next();
+    }
+    const injector = ctx.injector;
+    let msgQueue = injector.getInstance(RootMessageQueueToken);
     if (isClass(regIn)) {
         if (!injector.hasRegister(regIn)) {
             injector.registerType(regIn);
         }
         msgQueue = injector.getInstance(regIn);
     }
-    if (regIn !== 'none') {
-        if (before) {
-            msgQueue.useBefore(ctx.type, before);
-        } else if (after) {
-            msgQueue.useAfter(ctx.type, after);
-        } else {
-            msgQueue.use(ctx.type);
-        }
+
+    if (before) {
+        msgQueue.useBefore(ctx.type, before);
+    } else if (after) {
+        msgQueue.useAfter(ctx.type, after);
+    } else {
+        msgQueue.use(ctx.type);
     }
     next();
 };
