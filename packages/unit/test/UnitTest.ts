9bf1463f77f7d27441230ff512a3bd42e75fb058
houjun <freebirds8281@hotmail.com>
2020-10-14 22:54:38 +0800

    timeout reset

--- packages/unit/test/UnitTest.ts
+++ packages/unit/test/UnitTest.ts
@@ -23,18 +23,18 @@ export class SuiteTest {
         console.log('---------beofre test-----------');
     }
 
-    @Test('assert test timeout', 200)
+    @Test('assert test timeout', 50)
     testTimeout() {
         console.log('---------assert test timeout------');
         let def = PromiseUtil.defer();
         setTimeout(() => {
             console.log('out time do...');
             def.resolve('out time do...')
-        }, 100)
+        }, 10)
         return def.promise;
     }
 
-    @Test('assert test in time', 200)
+    @Test('assert test in time', 50)
     testInTime(assert: Assert) {
         console.log('---------assert test in time------');
         let def = PromiseUtil.defer();
@@ -42,7 +42,7 @@ export class SuiteTest {
         setTimeout(() => {
             console.log('in time do...');
             def.resolve('in time do...')
-        }, 100)
+        }, 10)
         return def.promise;
     }
 
