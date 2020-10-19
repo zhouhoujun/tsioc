2194526faa779d910b5e4f77da0f4ae6db7a2cee
houjun <freebirds8281@hotmail.com>
2020-10-19 23:25:06 +0800

    exports

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -10,7 +10,7 @@ export * from './bindings/IBinding';
 
 // change ref.
 export * from './chage/change';
-export * from './chage/view';
+export * from './chage/iterable';
 
 // compile
 export * from './compile/facade';
@@ -20,8 +20,11 @@ export * from './compile/interface';
 // directivies
 export * from './directivies/each';
 export * from './directivies/if';
-export * from './directivies/elseif';
+export * from './directivies/outlet';
 export * from './directivies/switch';
+
+// i18n
+export * from './i18n/tokens';
 
 // pipes
 export * from './pipes/pipe';
@@ -30,12 +33,23 @@ export * from './pipes/cases';
 export * from './pipes/slice';
 export * from './pipes/json';
 
+// refs
+export * from './refs/component';
+export * from './refs/container';
+export * from './refs/element';
+export * from './refs/template';
+export * from './refs/view';
+
+
 // registers
 export * from './registers/ComponentDefAction';
 export * from './registers/DirectiveDefAction';
 export * from './registers/PipeRegAction';
 
+// util
 export * from './util/array';
+export * from './util/stringify';
+
 export * from './context';
 export * from './reflect';
 export * from './lifecycle';
