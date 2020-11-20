f58c415bd3003e13ddce9eab05c742df82b6d480
houjun <freebirds8281@hotmail.com>
2020-11-20 16:47:52 +0800

    del

--- packages/component/src/directivies/if.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, Input } from '../decorators';
-import { ITemplateRef } from '../ComponentRef';
-
-@Directive('[if]')
-export class DirectiveIf {
-    @Input() if: any;
-    @Input() content: ITemplateRef;
-}
