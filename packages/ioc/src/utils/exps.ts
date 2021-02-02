ae848148694fa84402d7c2f55f6f546a28d9b791
houjun <freebirds8281@hotmail.com>
2021-02-02 08:51:46 +0800

    update

--- packages/ioc/src/utils/exps.ts
+++ packages/ioc/src/utils/exps.ts
@@ -1,4 +1,4 @@
-export const clsStartExp = /^[A-Z@]/;
+export const clsNameExp = /^[A-Z@]/;
 export const clsUglifyExp = /^[a-z0-9]$/;
 export const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 export const ARGUMENT_NAMES = /([^\s,]+)/g;
