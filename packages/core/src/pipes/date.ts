cc90f4bd1f17d75c0d32d5460e254bc81a469d11
houhou <freebirds8281@hotmail.com>
2022-04-11 16:54:57 +0800

    substring

--- packages/core/src/pipes/date.ts
+++ packages/core/src/pipes/date.ts
@@ -12,9 +12,9 @@ export function formatDate(date: Date, fmt: string = 'yyyy-MM-dd') {
         "q+": Math.floor((date.getMonth() + 3) / 3), //季度
         "S": date.getMilliseconds() //毫秒
     } as any;
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substring(4 - RegExp.$1.length));
     for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substring(("" + o[k]).length)));
     return fmt;
 }
 
