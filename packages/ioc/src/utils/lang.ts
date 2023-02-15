73cf3adfc616252767fd6a97848f7929b5adc5f7
houjun <freebirds8281@hotmail.com>
2023-02-15 09:20:51 +0800

    fix bug

--- packages/ioc/src/utils/lang.ts
+++ packages/ioc/src/utils/lang.ts
@@ -774,8 +774,8 @@ export namespace PromiseUtil {
      * @param {((T | PromiseLike<T> | ((value: T) => T | PromiseLike<T>))[])} promises
      * @returns
      */
-    export function step<T>(promises: (T | PromiseLike<T> | ((value: T) => T | PromiseLike<T>))[]) {
-        let result = Promise.resolve<T>(null);
+    export function step<T>(promises: (T | PromiseLike<T> | ((value: T) => T | PromiseLike<T>))[]): Promise<T> {
+        let result: Promise<T> = Promise.resolve<T>(null);
         promises.forEach(p => {
             result = result.then(v => isFunction(p) ? p(v) : p);
         });
