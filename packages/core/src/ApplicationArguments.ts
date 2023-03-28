8b1d5a378d551dbadea867225109b4202dcc4e40
houjun <freebirds8281@hotmail.com>
2023-03-28 09:44:51 +0800

    args

--- packages/core/src/ApplicationArguments.ts
+++ packages/core/src/ApplicationArguments.ts
@@ -3,31 +3,31 @@ import { Abstract } from '@tsdi/ioc';
 
 /** 
  * application arguments.  
- */ 
+ */
 @Abstract()
 export abstract class ApplicationArguments implements Record<string, any> {
     /** 
      * process args source  
-     */ 
+     */
     abstract get argsSource(): string[];
     /** 
      * process args map. 
-     */ 
+     */
     abstract get args(): Record<string, string>;
     /**
      * process args command. 
      * 
-     */ 
+     */
     abstract get cmds(): string[];
 
     /**
      * process env 
-     */ 
+     */
     abstract get env(): Record<string, string | undefined>;
 
     /**
      * process exit signls. 
-     */ 
+     */
     abstract get signls(): string[];
 
     /** 
@@ -35,5 +35,5 @@ export abstract class ApplicationArguments implements Record<string, any> {
      * @param args the args reset with. 
      */
     abstract reset(args: string[]): void;
-} 
+}
 
