216f306274277cd32a9de504446d2c53d2004ed6
houjun <freebirds8281@hotmail.com>
2023-05-25 11:08:30 +0800

    cleanup

--- packages/transport-tcp/src/microservice/endpoint.ts
+++
@@ -1,7 +0,0 @@
-// import { Outgoing, TransportEndpoint, TransportContext } from '@tsdi/core';
-// import { Abstract } from '@tsdi/ioc';
-
-// @Abstract()
-// export abstract class TcpMicroEndpoint extends TransportEndpoint<TransportContext, Outgoing> {
-
-// }
