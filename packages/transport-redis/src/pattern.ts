954da067e3554fa29fcb786495401b090b28adcc
zhouhoujun <freebirds8281@hotmail.com>
2023-06-27 19:04:48 +0800

    redis pattern formatter nomalize

--- packages/transport-redis/src/pattern.ts
+++ packages/transport-redis/src/pattern.ts
@@ -1,9 +1,9 @@
-import { Pattern, PatternFormatter, patternToPath } from '@tsdi/core';
+import { Pattern, PatternFormatter, normalize, patternToPath } from '@tsdi/core';
 import { Injectable } from '@tsdi/ioc';
 
 @Injectable()
 export class RedisPatternFormatter extends PatternFormatter {
     format(pattern: Pattern): string {
-        return patternToPath(pattern, '/', '=')
+        return normalize(patternToPath(pattern, '/', '='))
     }
 }
