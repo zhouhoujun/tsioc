b9051f943ac925a804245590df17bca068a639d9
houjun <freebirds8281@hotmail.com>
2023-07-20 17:52:21 +0800

    send with header

--- packages/transport-ws/src/transport.ts
+++ packages/transport-ws/src/transport.ts
@@ -30,91 +30,152 @@ export class WsTransportSessionFactoryImpl implements WsTransportSessionFactory 
 
 export class WsTransportSession extends SocketTransportSession<Duplex> {
 
-    maxSize = 1024 * 256 - 6 - 3;
-
-    write(packet: Subpackage, chunk: Buffer | null, callback?: ((err?: any) => void) | undefined): void {
-        
+    maxSize = 1024 * 256 - 6;
+    write(packet: Subpackage, chunk: Buffer, callback?: ((err?: any) => void) | undefined): void {
         if (!packet.headerSent) {
             this.generateHeader(packet)
                 .then((buff) => {
-                    this.socket.write(buff, (err) => {
+                    if (this.hasPayloadLength(packet)) {
+                        packet.residueSize = packet.payloadSize ?? 0;
+                        packet.caches = [buff];
+                        packet.cacheSize = Buffer.byteLength(buff);
                         packet.headerSent = true;
-                        if (!err && chunk) {
-                            this.write(packet, chunk, callback);
+                        packet.headCached = true;
+                        if (chunk) {
+                            this.write(packet, chunk, callback)
                         } else {
-                            callback?.(err);
+                            callback?.();
                         }
-                    })
+                    } else {
+                        this.socket.write(buff, callback);
+                    }
                 })
                 .catch(err => callback?.(err))
             return;
         }
+
         if (!chunk) throw new ArgumentExecption('chunk can not be null!');
 
 
-        const pkgSize = packet.payloadSize ?? 0;
-        if (pkgSize <= this.maxSize) {
-            if (!packet.payloadSent) {
-                const prefix = this.getPayloadPrefix(packet, packet.payloadSize!);
-                packet.payloadSent = true;
-                this.socket.write(chunk ? Buffer.concat([prefix, chunk]) : chunk, callback)
-            } else {
-                this.socket.write(chunk, callback)
-            }
-        } else {
-            if (isNil(packet.residueSize)) {
-                packet.residueSize = packet.payloadSize ?? 0;
-            }
-            if (isNil(packet.cacheSize)) {
-                packet.cacheSize = 0;
-            }
-            if (isNil(packet.caches)) {
-                packet.caches = [];
-            }
-
-            const bufSize = Buffer.byteLength(chunk);
-
-            const tol = packet.cacheSize + bufSize;
-            if (tol == this.maxSize) {
-                const prefix = this.getPayloadPrefix(packet, this.maxSize);
-                packet.caches.push(chunk);
-                const data = Buffer.concat([prefix, ...packet.caches]);
-                packet.residueSize -= bufSize;
-                packet.caches = [];
-                packet.cacheSize = 0;
-                this.socket.write(data, callback);
-            } else if (tol > this.maxSize) {
-                const idx = bufSize - (tol - this.maxSize);
-                const message = chunk.subarray(0, idx);
-                const rest = chunk.subarray(idx);
-                const prefix = this.getPayloadPrefix(packet, this.maxSize);
-                packet.caches.push(message);
-                const data = Buffer.concat([prefix, ...packet.caches]);
-                packet.residueSize -= (bufSize - rest.length)
-                packet.caches = [];
-                packet.cacheSize = 0;
-                this.socket.write(data, (err) => {
-                    if (err) throw err;
-                    if (rest.length) {
-                        this.write(packet, rest, callback)
-                    }
-                })
-            } else {
-                packet.caches.push(chunk);
-                packet.cacheSize += bufSize;
-                packet.residueSize -= bufSize;
-                if (packet.residueSize <= 0) {
-                    const prefix = this.getPayloadPrefix(packet, packet.cacheSize);
-                    const data = Buffer.concat([prefix, ...packet.caches]);
-                    packet.caches = [];
-                    packet.cacheSize = 0;
-                    this.socket.write(data, callback);
-                } else if (callback) {
-                    callback()
+        const bufSize = Buffer.byteLength(chunk);
+        const maxSize = this.maxSize - (packet.headCached ? 6 : 3);
+    
+        const tol = packet.cacheSize + bufSize;
+        if (tol == maxSize) {
+            packet.caches.push(chunk);
+            const data = this.getSendBuffer(packet, maxSize);
+            packet.residueSize -= bufSize;
+            this.socket.write(data, callback);
+        } else if (tol > maxSize) {
+            const idx = bufSize - (tol - maxSize);
+            const message = chunk.subarray(0, idx);
+            const rest = chunk.subarray(idx);
+            packet.caches.push(message);
+            const data = this.getSendBuffer(packet, maxSize);
+            packet.residueSize -= (bufSize - Buffer.byteLength(rest));
+            this.socket.write(data, (err) => {
+                if (err) throw err;
+                if (rest.length) {
+                    this.write(packet, rest, callback)
                 }
+            })
+        } else {
+            packet.caches.push(chunk);
+            packet.cacheSize += bufSize;
+            packet.residueSize -= bufSize;
+            if (packet.residueSize <= 0) {
+                const data = this.getSendBuffer(packet, packet.cacheSize);
+                this.socket.write(data, callback);
+            } else if (callback) {
+                callback()
             }
         }
     }
+
+    // maxSize = 1024 * 256 - 6 -3;
+    // write(packet: Subpackage, chunk: Buffer | null, callback?: ((err?: any) => void) | undefined): void {
+        
+    //     if (!packet.headerSent) {
+    //         this.generateHeader(packet)
+    //             .then((buff) => {
+    //                 this.socket.write(buff, (err) => {
+    //                     packet.headerSent = true;
+    //                     if (!err && chunk) {
+    //                         this.write(packet, chunk, callback);
+    //                     } else {
+    //                         callback?.(err);
+    //                     }
+    //                 })
+    //             })
+    //             .catch(err => callback?.(err))
+    //         return;
+    //     }
+    //     if (!chunk) throw new ArgumentExecption('chunk can not be null!');
+
+
+    //     const pkgSize = packet.payloadSize ?? 0;
+    //     if (pkgSize <= this.maxSize) {
+    //         if (!packet.payloadSent) {
+    //             const prefix = this.getPayloadPrefix(packet, packet.payloadSize!);
+    //             packet.payloadSent = true;
+    //             this.socket.write(chunk ? Buffer.concat([prefix, chunk]) : chunk, callback)
+    //         } else {
+    //             this.socket.write(chunk, callback)
+    //         }
+    //     } else {
+    //         if (isNil(packet.residueSize)) {
+    //             packet.residueSize = packet.payloadSize ?? 0;
+    //         }
+    //         if (isNil(packet.cacheSize)) {
+    //             packet.cacheSize = 0;
+    //         }
+    //         if (isNil(packet.caches)) {
+    //             packet.caches = [];
+    //         }
+
+    //         const bufSize = Buffer.byteLength(chunk);
+
+    //         const tol = packet.cacheSize + bufSize;
+    //         if (tol == this.maxSize) {
+    //             const prefix = this.getPayloadPrefix(packet, this.maxSize);
+    //             packet.caches.push(chunk);
+    //             const data = Buffer.concat([prefix, ...packet.caches]);
+    //             packet.residueSize -= bufSize;
+    //             packet.caches = [];
+    //             packet.cacheSize = 0;
+    //             this.socket.write(data, callback);
+    //         } else if (tol > this.maxSize) {
+    //             const idx = bufSize - (tol - this.maxSize);
+    //             const message = chunk.subarray(0, idx);
+    //             const rest = chunk.subarray(idx);
+    //             const prefix = this.getPayloadPrefix(packet, this.maxSize);
+    //             packet.caches.push(message);
+    //             const data = Buffer.concat([prefix, ...packet.caches]);
+    //             packet.residueSize -= (bufSize - rest.length)
+    //             packet.caches = [];
+    //             packet.cacheSize = 0;
+    //             this.socket.write(data, (err) => {
+    //                 if (err) throw err;
+    //                 if (rest.length) {
+    //                     this.write(packet, rest, callback)
+    //                 }
+    //             })
+    //         } else {
+    //             packet.caches.push(chunk);
+    //             packet.cacheSize += bufSize;
+    //             packet.residueSize -= bufSize;
+    //             if (packet.residueSize <= 0) {
+    //                 const prefix = this.getPayloadPrefix(packet, packet.cacheSize);
+    //                 const data = Buffer.concat([prefix, ...packet.caches]);
+    //                 packet.caches = [];
+    //                 packet.cacheSize = 0;
+    //                 this.socket.write(data, callback);
+    //             } else if (callback) {
+    //                 callback()
+    //             }
+    //         }
+    //     }
+    // }
 
     protected handleFailed(error: any): void {
         this.socket.emit(ev.ERROR, error.message);
