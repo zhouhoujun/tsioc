e2be9df99decbcbc5640f1660816a41b5f500157
zhouhoujun <freebirds8281@hotmail.com>
2024-06-20 18:32:52 +0800

    date

--- packages/core/src/pipes/formats/date.ts
+++ packages/core/src/pipes/formats/date.ts
@@ -6,6 +6,24 @@ import { PipeTransform, invalidPipeArgument } from './../pipe';
  * format date.
  * @param date 
  * @param fmt 
+ *  
+ * 
+ * * datetime: `yyyy-MM-dd hh:mm:ss`
+ * * weeks: `yyyy-Ww`
+ * * date: `yyyy-MM-dd`
+ * * time: `hh:mm:ss`
+ * 
+ * ```
+ * y: 年
+ * M: 月份
+ * d: 日
+ * h: 小时
+ * m: 分
+ * q: 季度
+ * w: 周
+ * s: 秒
+ * S: 毫秒
+ * ```
  * @returns 
  */
 export function formatDate(date: Date, fmt = 'yyyy-MM-dd') {
@@ -16,7 +34,8 @@ export function formatDate(date: Date, fmt = 'yyyy-MM-dd') {
         "m+": date.getMinutes(), //分
         "s+": date.getSeconds(), //秒
         "q+": Math.floor((date.getMonth() + 3) / 3), //季度
-        "S": date.getMilliseconds() //毫秒
+        'w+': getWeek(date), //周
+        "S": date.getMilliseconds(), //毫秒
     } as any;
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substring(4 - RegExp.$1.length));
     for (const k in o)
@@ -24,8 +43,31 @@ export function formatDate(date: Date, fmt = 'yyyy-MM-dd') {
     return fmt
 }
 
+function getWeek(date: Date): number {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);  // 获取当年的第一天
+    const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;  // 计算已经过去的天数
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);  // 计算当前日期是第几周
+}
+
 /**
  * date format pipe.
+ * 
+ * * datetime: `yyyy-MM-dd hh:mm:ss`
+ * * weeks: `yyyy-Ww`
+ * * date: `yyyy-MM-dd`
+ * * time: `hh:mm:ss`
+ * 
+ * ```
+ * y: 年
+ * M: 月份
+ * d: 日
+ * h: 小时
+ * m: 分
+ * q: 季度
+ * w: 周
+ * s: 秒
+ * S: 毫秒
+ * ```
  */
 @Pipe('date-format')
 export class DateFormatPipe implements PipeTransform<string> {
