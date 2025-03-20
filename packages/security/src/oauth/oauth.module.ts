dadfa0f01d27151d59974596bf0e6ec0a11d93ad
zhouhoujun <80599915@qq.com>
2025-03-20 18:10:39 +0800

    oauth

--- packages/security/src/oauth/oauth.module.ts
+++ packages/security/src/oauth/oauth.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@tsdi/ioc';
+import { JWTModule } from '../jwt';
 
 @Module({
+    imports:[
+        JWTModule
+    ],
+    providers:[
 
+    ]
 })
 export class OAuthModule {
 
