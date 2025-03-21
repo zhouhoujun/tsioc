f93f001a4ca23d4dba0f64f0bb1e3aef7a846a98
zhouhoujun <80599915@qq.com>
2025-03-21 18:26:00 +0800

    jwt

--- packages/security/src/jwt/jwt.interceptor.ts
+++ packages/security/src/jwt/jwt.interceptor.ts
@@ -1,18 +1,65 @@
 import * as jwt from 'jsonwebtoken';
 
-import { Abstract, Injectable } from '@tsdi/ioc';
+import { Injectable, lang } from '@tsdi/ioc';
 import { ApplicationHandler, ApplicationInterceptor } from '@tsdi/core';
-import { OutgoingMessage } from '@tsdi/common/transport';
-import { RequestContext, Session } from '@tsdi/endpoints';
-import { Observable } from 'rxjs';
+import { Incoming, OutgoingMessage, UnauthorizedExecption } from '@tsdi/common/transport';
+import { RequestContext, SessionManager } from '@tsdi/endpoints';
+import { defer, mergeMap, Observable, throwError } from 'rxjs';
+import { InvalidTokenExecption } from '../exceptions';
 
+
+export class JWTOption {
+    constructor(
+        readonly tokenName: string,
+        readonly secret: jwt.Secret | jwt.PublicKey,
+        readonly tokenIn: 'header' | 'query' | 'body' = 'header',
+        readonly options?: jwt.VerifyOptions) { }
+}
 
 @Injectable()
 export class JwtInterceptor implements ApplicationInterceptor<RequestContext, OutgoingMessage> {
 
     intercept(input: RequestContext, next: ApplicationHandler, context?: any): Observable<any> {
-        return next.handle(input, context)
+        const option = input.get(JWTOption);
+        const token = this.getToken(input, option);
+        if (!token) return throwError(() => new InvalidTokenExecption('no token'));
+
+        return defer(() => {
+            const defer = lang.defer();
+            jwt.verify(token, option.secret, option.options, (err, decoded) => {
+                if (err) {
+                    defer.reject(new InvalidTokenExecption(err.message));
+                }
+                input.get(SessionManager).login(decoded)
+                    .then(() => {
+                        defer.resolve();
+                    }).catch(err => {
+                        defer.reject(new InvalidTokenExecption(err.message));
+                    })
+            });
+            return defer.promise;
+        }).pipe(
+            mergeMap(() => next.handle(input, context))
+        );
     }
 
+    getToken(ctx: RequestContext, option: JWTOption) {
+        switch (option.tokenIn) {
+            case 'header':
+                return parseAuthHeader(ctx.getHeader(option.tokenName))?.value;
+            case 'query':
+                return ctx.query[option.tokenName];
+            case 'body':
+                return ctx.request.body[option.tokenName];
+        }
+    }
 }
 
+const matcExp = /(\S+)\s+(\S+)/;
+function parseAuthHeader(hdrValue?: string) {
+    if (typeof hdrValue !== 'string') {
+        return null;
+    }
+    var matches = hdrValue.match(matcExp);
+    return matches && { scheme: matches[1], value: matches[2] };
+}
