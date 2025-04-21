2c49540f277c9d74bf263d0e4e20832bfb4fc8d8
zhouhoujun <80599915@qq.com>
2025-04-21 14:37:53 +0800

    clean

--- packages/components/src/components.ts
+++ packages/components/src/components.ts
@@ -1,11 +1,4 @@
 import { Module } from '@tsdi/ioc';
-import { ForDirective } from './directivies/for';
-import { IfDirective } from './directivies/if';
-import { TemplateOutletDirective } from './directivies/outlet';
-import { Plural, PluralCase } from './directivies/plural';
-import { DirSwitch, DirSwitchCase, DirSwitchDefault } from './directivies/switch';
-import { AsyncPipe } from './pipes/async';
-import { ComponentState } from './state';
 
 
 
@@ -16,19 +9,11 @@ import { ComponentState } from './state';
  * @class ComponentsModule
  */
 @Module({
-    providers:[
-        ComponentState
+    providers: [
+        
     ],
-    exports:[
-        DirSwitch,
-        DirSwitchCase,
-        DirSwitchDefault,
-        ForDirective,
-        IfDirective,
-        TemplateOutletDirective,
-        Plural,
-        PluralCase,
-        AsyncPipe
+    exports: [
+        
     ]
 })
 export class ComponentsModule {
