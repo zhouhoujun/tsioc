d2c2c79ad36e8eda13f57d5641567f61db2a11d2
zhouhoujun <80599915@qq.com>
2025-05-19 15:27:04 +0800

    context

--- packages/swagger/src/swagger.module.ts
+++ packages/swagger/src/swagger.module.ts
@@ -3,7 +3,7 @@ import { SWAGGER_SETUP_OPTIONS, SwaggerSetupOptions } from './swagger.config';
 import { SwaggerService } from './swagger.service';
 
 @Module({
-    declarations: [
+    providers: [
         // SwaggerJson,
         SwaggerService
     ]
