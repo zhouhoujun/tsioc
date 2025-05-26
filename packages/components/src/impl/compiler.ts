e85c878fb9cf60634f9a808862e48f325dcf1623
zhouhoujun <80599915@qq.com>
2025-05-26 17:38:17 +0800

    focas endpoint

--- packages/components/src/impl/compiler.ts
+++ packages/components/src/impl/compiler.ts
@@ -1,5 +1,5 @@
 import { isObject } from '@tsdi/ioc';
-// import { XMLBuilder, XMLParser } from 'fast-xml-parser';
+import { XMLBuilder, XMLParser } from 'fast-xml-parser';
 import { ReactiveEffect } from '../ReactiveEffect';
 import { TemplateCompiler, TemplateCompilerOptions } from '../template/compiler';
 
