611f9eff130d5f318cfe0c5e5f5333e0b583c19b
zhouhoujun <80599915@qq.com>
2025-09-02 17:59:22 +0800

    components

--- packages/components/src/decorators/component.ts
+++ packages/components/src/decorators/component.ts
@@ -1,7 +1,8 @@
-import { ModuleType, createDecorator, AnnotationType, noPointcut, getModuleType, TypeDef, ActionTypes } from '@tsdi/ioc';
+import { ModuleType, createDecorator, AnnotationType, noPointcut, getModuleType, TypeDef, ActionTypes, InvocationContext } from '@tsdi/ioc';
 import { ComponentFactory } from '../refs/component';
 import { Attribute, AttributeMetadata } from './atteribute';
 import { SchemaMetadata } from '../template/schema';
+import { COMPONENTS, DIRECTIVES } from '../template/compiler';
 
 export interface ComponentDef<T = any> extends TypeDef<T> {
     imports?: ModuleType[],
@@ -27,10 +28,30 @@ export const Component: ComponentDecorator = createDecorator<Partial<ComponentDe
             (typeRef.type as AnnotationType)[noPointcut] = true;
             typeRef.setAnnotation(ctx.define.metadata);
             const def = typeRef.getAnnotation<ComponentDef>();
+            if (!def.selector) def.selector = typeRef.className;
             const metadata = ctx.define.metadata;
             def.providers = metadata.providers;
             if (metadata.imports) def.imports = getModuleType(metadata.imports);
             def.attributes = typeRef.getDefines(Attribute).map(d => d as AttributeMetadata);
+        }
+    },
+    design: {
+        afterAnnoation: (ctx) => {
+            const typeRef = ctx.class;
+            const def = typeRef.getAnnotation<ComponentDef>();
+            if (!def.selector) return;
+            const selectors = def.selector.split(',');
+            const factory = ctx.injector.get(ComponentFactory);
+            // for (let sel of selectors) {
+            //     sel = sel.trim();
+            //     const func = (parent: InvocationContext) => factory.create(typeRef, { parent });
+            //     func['name'] = sel;
+            //     if (sel.indexOf('[') > -1) {
+            //         ctx.injector.inject({ provide: DIRECTIVES, useValue: func, multi: true });
+            //     } else {
+            //         ctx.injector.inject({ provide: COMPONENTS, useValue: func, multi: true });
+            //     }
+            // }
         }
     },
     factory: (injector) => {
