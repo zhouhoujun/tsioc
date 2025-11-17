f64c437e15208b09b0a771e5714b989fb795c9d3
zhouhoujun <80599915@qq.com>
2025-11-17 13:13:32 +0800

    for

--- packages/ioc/src/utils/lang.ts
+++ packages/ioc/src/utils/lang.ts
@@ -102,7 +102,8 @@ export function deepForEach<T>(
     isRecord?: (value: any) => boolean,
     getRecord?: (value: any) => T[]): void | Promise<void> {
     const ps: Promise<void>[] = [];
-    for (const value of input) {
+    for (let i = 0, len = input.length; i < len; i++) {
+        const value = input[i];
         if (isArray(value)) {
             const reslut = deepForEach(value, fn, isRecord, getRecord);
             if (reslut) {
