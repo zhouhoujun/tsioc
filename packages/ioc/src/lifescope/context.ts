888095821320cf741648f7ec4b25dab5d4e21d83
zhouhoujun <80599915@qq.com>
2025-11-25 13:35:42 +0800

    context

--- packages/ioc/src/lifescope/context.ts
+++ packages/ioc/src/lifescope/context.ts
@@ -24,6 +24,34 @@ const MUTIL = new ContextToken<boolean>(() => false);
 
 export class IocContext extends ContextAdapter {
 
+    get runtime(): Runtime {
+        return this.get(Runtime);
+    }
+
+    set runtime(value: Runtime) {
+        this.set(Runtime, value);
+    }
+
+    get injector(): Injector {
+        return this.get(INJECTOR);
+    }
+
+    set injector(value: Injector) {
+        this.set(INJECTOR, value);
+    }
+
+
+    /**
+     * the token to provide.
+     */
+    get provide(): Token | null {
+        return this.get(PROVIDE);
+    }
+
+    set provide(value: Token | null) {
+        this.set(PROVIDE, value);
+    }
+
     /**
      * whether the context is mutil.
      */
@@ -35,14 +63,6 @@ export class IocContext extends ContextAdapter {
         this.set(MUTIL, value);
     }
 
-    get runtime(): Runtime {
-        return this.get(Runtime);
-    }
-
-    set runtime(value: Runtime) {
-        this.set(Runtime, value);
-    }
-
     get currDecor(): DecoratorFn | null {
         return this.get(CURR_DECOR);
     }
@@ -50,6 +70,42 @@ export class IocContext extends ContextAdapter {
     set currDecor(value: DecoratorFn | null) {
         this.set(CURR_DECOR, value);
     }
+
+}
+
+
+export class RuntimeContext extends IocContext {
+
+    /**
+     * constructor parameters.
+     */
+    get params(): Parameters {
+        return this.get(CTOR_PARAMS);
+    }
+
+    set params(value: Parameters) {
+        this.set(CTOR_PARAMS, value);
+    }
+
+    /**
+     * constructor arguments.
+     */
+    get args(): any[] | null {
+        return this.get(CTOR_ARGS);
+    }
+
+    set args(value: any[] | null) {
+        this.set(CTOR_ARGS, value);
+    }
+
+    get raiseInjector(): Injector {
+        return this.get(RAISE_INJECTOR);
+    }
+
+    set raiseInjector(value: Injector) {
+        this.set(RAISE_INJECTOR, value);
+    }
+
 
     get instance(): any {
         return this.get(INSTANCE);
@@ -69,84 +125,29 @@ export class IocContext extends ContextAdapter {
     set isResolve(value: boolean) {
         this.set(IS_RESOLVE, value);
     }
-
-    /**
-     * the token to provide.
-     */
-    get provide(): Token | null {
-        return this.get(PROVIDE);
-    }
-
-    set provide(value: Token | null) {
-        this.set(PROVIDE, value);
-    }
-
-    get injector(): Injector {
-        return this.get(INJECTOR);
-    }
-
-    set injector(value: Injector) {
-        this.set(INJECTOR, value);
-    }
-
-    /**
-     * constructor arguments.
-     */
-    get args(): any[] | null {
-        return this.get(CTOR_ARGS);
-    }
-
-    set args(value: any[] | null) {
-        this.set(CTOR_ARGS, value);
-    }
-
-    /**
-     * constructor parameters.
-     */
-    get params(): Parameters {
-        return this.get(CTOR_PARAMS);
-    }
-
-    set params(value: Parameters) {
-        this.set(CTOR_PARAMS, value);
-    }
-
 }
 
 
-export class RuntimeContext extends IocContext {
-
-
-    get raiseInjector(): Injector {
-        return this.get(RAISE_INJECTOR);
+export function createContext(injector: Injector, previous?: Context, runtime?: Runtime, multi?: boolean, provide?: Token | null) {
+    const context = new IocContext(previous);
+    context.set(INJECTOR, injector);
+    if (!context.has(Runtime)) context.set(Runtime, runtime ?? injector.getRuntime());
+    if (multi) context.set(MUTIL, true);
+    if (!multi && provide) {
+        context.set(PROVIDE, provide);
     }
-
-    set raiseInjector(value: Injector) {
-        this.set(RAISE_INJECTOR, value);
-    }
+    return context;
 }
 
+export function createRuntimeContext(injector: Injector, previous?: Context, runtime?: Runtime, raise?: Injector, multi?: boolean, params?: Parameters) {
+    const context = new RuntimeContext(previous);
+    context.set(INJECTOR, injector);
+    if (!context.has(Runtime)) context.set(Runtime, runtime ?? injector.getRuntime());
 
- export function createContext(injector: Injector, previous?: Context, runtime?: Runtime, multi?: boolean, provide?: Token | null) {
-        const context= new IocContext(previous);
-        context.set(INJECTOR, injector);
-        if(!context.has(Runtime)) context.set(Runtime, runtime ?? injector.getRuntime());
-        if (multi) context.set(MUTIL, true);
-        if (!multi && provide) {
-            context.set(PROVIDE, provide);
-        }
-        return context;
+    context.set(RAISE_INJECTOR, raise || injector);
+    if (multi) context.set(MUTIL, true);
+    if (params) {
+        context.set(CTOR_PARAMS, params);
     }
-
-    export function createRuntimeContext(injector: Injector, previous?: Context, runtime?: Runtime, raise?: Injector, multi?: boolean, params?: Parameters) {
-        const context= new RuntimeContext(previous);
-        context.set(INJECTOR, injector);
-        if(!context.has(Runtime)) context.set(Runtime, runtime ?? injector.getRuntime());
-        
-        context.set(RAISE_INJECTOR, raise || injector);
-        if (multi) context.set(MUTIL, true);
-        if (params) {
-            context.set(CTOR_PARAMS, params);
-        }
-        return context;
-    }
+    return context;
+}
