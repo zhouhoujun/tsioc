2f577c26d459e504447707e2ddb1e82ccb41b3a0
zhouhoujun <80599915@qq.com>
2025-12-19 17:59:20 +0800

    force to convert request backend to Observable

--- packages/common/src/handler.ts
+++ packages/common/src/handler.ts
@@ -164,11 +164,10 @@ export class DefaultRequestHandler<
         return toObservable(super.handle(input, context)) as Observable<TRes>;
     }
 
-
-    protected override composeFilterFn(filters: FilterLike[]): FilterFn {
-        const fn = composeFilters(filters);
-        return (input: any, next: HandlerFn, context?: any) => toObservable(fn(input, next, context));
-    }
+    // protected override composeFilterFn(filters: FilterLike[]): FilterFn {
+    //     const fn = composeFilters(filters);
+    //     return (input: any, next: HandlerFn, context?: any) => toObservable(fn(input, next, context));
+    // }
 
     protected override generateInterceptorFn(fns: InterceptorLike[]): InterceptorFn {
         const options = this.options as RequestHandlerOptions;
@@ -180,16 +179,16 @@ export class DefaultRequestHandler<
     }
 
     protected override generateBackendFn(): HandlerFn {
-        const handler = super.generateBackendFn();
+        const handle = super.generateBackendFn();
         const options = this.options as RequestHandlerOptions;
         if (options.side === TransferSide.client) {
             const transfers = this.context.get(options.transfersToken!);
             if (transfers?.length) {
-                return composeToHanlderFn(handler, transfers)
+                return composeToHanlderFn(handle, transfers)
             }
         }
 
-        return handler;
+        return (req: TReq, context: TContext) => toObservable(handle(req, context));
     }
 
     protected override forbiddenError(): Exception {
