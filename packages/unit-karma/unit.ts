0826be5a63ac1d8cca9fc5a5a73d7ac98eeca5ab
zhouhoujun <freebirds8281@hotmail.com>
2026-04-08 22:29:35 +0800

    test

--- packages/unit-karma/unit.ts
+++ packages/unit-karma/unit.ts
@@ -1,22 +1,5 @@
 import { runTest } from '@tsdi/unit';
 import { CoverageReporter } from './src';
 
-const args = process.argv.slice(2);
-const withCoverage = args.includes('-c') || args.includes('--coverage');
 
-if (withCoverage) {
-    const outputDir = process.env.NODE_V8_COVERAGE || '.nyc_output';
-    runTest('./test/**/*.ts', {
-        baseURL: __dirname,
-        platform: 'browser',
-        coverage: {
-            enabled: true,
-            reporters: ['text', 'text-summary', 'lcov', 'cobertura'],
-            include: ['**/src/**/*.ts'],
-            exclude: ['**/test/**', '**/*.spec.ts'],
-            outputDir: outputDir
-        }
-    }, CoverageReporter);
-} else {
-    runTest('./test/**/*.ts', { baseURL: __dirname, platform: 'browser' }, CoverageReporter);
-}
+runTest('./test/**/*.ts', { baseURL: __dirname, platform: 'browser' }, CoverageReporter);
