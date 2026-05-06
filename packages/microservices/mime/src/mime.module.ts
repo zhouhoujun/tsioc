5d7b2f303f7b64afeb4187a07bea1f798f591d68
zhouhoujun <80599915@qq.com>
2026-05-06 17:25:12 +0800

    mine

---
+++ packages/microservices/mime/src/mime.module.ts
@@ -0,0 +1,17 @@
+import { MimeAdapter, MimeDb, MimeTypes, AcceptsPriority } from '@tsdi/common';
+import { Module } from '@tsdi/ioc';
+import { MimeAdapterImpl, MimeTypesImpl } from './impl/mime';
+import { BasicMimeDb } from './impl/mimedb';
+import { AcceptsPriorityImpl } from './impl/accepts';
+
+@Module({
+    providers: [
+        { provide: MimeTypes, useClass: MimeTypesImpl },
+        { provide: MimeDb, useClass: BasicMimeDb },
+        { provide: MimeAdapter, useClass: MimeAdapterImpl },
+        { provide: AcceptsPriority, useClass: AcceptsPriorityImpl }
+    ]
+})
+export class MimeModule {
+
+}
---
+++ packages/microservices/mime/unit.ts
@@ -0,0 +1,5 @@
+// for unit test debug.
+
+import { runTest } from '@tsdi/unit';
+
+runTest('./test/**/*.ts', { baseURL: __dirname });
